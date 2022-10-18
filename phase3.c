d47739a691fd4f16aaf344b9050d8bd369ffbbf7
Dyllon Enos <81198557+dyllonenos@users.noreply.github.com>
2022-10-18 15:32:14 -0700

    Update phase3.c

--- phase3.c
+++ phase3.c
@@ -1,6 +1,11 @@
 /**
  * @file phase3.c
- * @author Nicholas Eng, Dyllon Enos
+ * @author Nicholas Eng and Dyllon Enos
+ * @brief 
+ * @version 0.1
+ * @date 2022-10-14
+ * 
+ * @copyright Copyright (c) 2022
  * 
  */
 
@@ -12,27 +17,33 @@
 #include <usloss.h>
 #include <usyscall.h>
 
+int currentSemID = 1;
+int currentPID = 1;
 
-// TODO: Handle SyscallIntVec[]
-
-typedef struct Semaphore Semaphore;
-
-struct Semaphore
+void syscallHandler(USLOSS_Sysargs *args);
+typedef struct Process Process;
+struct Process
 {
     int id;
-    int value;
-    //int dangerCheck;
+    char *name;
+    int (*func)(char*);
+    char *arg;
+    int stack_size;
+    int priority;
 };
 
-int currentID = 1;
-
 /**
- * @brief TODO
+ * @brief This function will set all of the elements
+ * of the system call array with our syscallHandler()
+ * function.
  * 
  */
 void phase3_init(void)
 {
-
+    for (int index = 0; index < MAXSYSCALLS; index++)
+    {
+        systemCallVec[index] = &syscallHandler;
+    }
 }
 
 /**
@@ -41,27 +52,28 @@ void phase3_init(void)
  */
 void phase3_start_service_processes(void)
 {
-
 }
 
-
-// spawn handler
 void syscallHandler(USLOSS_Sysargs *args)
 {
+    int old = USLOSS_PsrGet();
+    USLOSS_PsrSet(2);
+    // Spawn() syscall
     if (args->number == SYS_SPAWN)
-    {
-        int result = fork1(args->arg5, args->arg1, args->arg2, args->arg3, args->arg4);
-        if (result <= 0)
-        {
-            args->arg1 = -1;
-            args->arg4 = -1;
-        }
-        args->arg1 = result;
-        args->arg4 = 0;
+    {       
+        // int result = fork1(args->arg5, args->arg1, args->arg2, args->arg3, args->arg4);
+        int (*func)(char *) = args->arg1;
+        char *arg = args->arg2;
+        
+        func(arg);
+        // USLOSS_PsrSet(old);
+        currentPID += 1;
     }
 
-    if (args->number == SYS_WAIT)
+    // Wait() syscall
+    else if (args->number == SYS_WAIT)
     {
+        USLOSS_PsrSet(old);
         int status = 0;
         int result = join(&status);
         args->arg1 = result;
@@ -72,12 +84,15 @@ void syscallHandler(USLOSS_Sysargs *args)
         {
             args->arg4 = -2;
         }
+        
         return 0;
 
     }
 
-    if (args->number == SYS_TERMINATE)
+    // Terminate() syscall
+    else if (args->number == SYS_TERMINATE)
     {
+        USLOSS_PsrSet(old);
         // Joins until we removed all processes
         while (join(args->arg1) != -2)
         {
@@ -86,41 +101,59 @@ void syscallHandler(USLOSS_Sysargs *args)
         quit(args->arg1);
     }
 
-    if (args->number == SYS_SEMCREATE)
+    // SemCreate() syscall
+    else if (args->number == SYS_SEMCREATE)
     {
-        // TDOD
-        Semaphore new;
-        new.id = currentID;
-        new.value = args->arg1;
+        // TODO
         args->arg4 = 0;
         if (args->arg1 < 0)
         {
             args->arg4 = -1;
         }
-        currentID++;
+        args->arg1 = currentSemID;
+        currentSemID++;
     }
 
-    if (args->number == SYS_SEMP)
+    // SemP() syscall
+    else if (args->number == SYS_SEMP)
     {
-        // TODO
+        while (args->arg1 == 0)
+        {
+            ;
+        }
+        args->arg1 -= 1;
+        args->arg4 = 0;
+        if (args->arg1 < 1)
+        {
+            args->arg4 = -1;
+        }
     }
 
-    if (args->number == SYS_SEMV)
+    // SemV() syscall
+    else if (args->number == SYS_SEMV)
     {
-        // TODO
+        args->arg1 += 1;
+        args->arg4 = 0;
+        if (args->arg1 < 1)
+        {
+            args->arg4 = -1;
+        }
     }
-    
-    if (args->number == SYS_GETTIMEOFDAY)
+
+    // GetTimeofDay() syscall
+   else if (args->number == SYS_GETTIMEOFDAY)
     {
         args->arg1 = currentTime();
     }
 
-    if (args->number == SYS_GETPROCINFO)
+    // CPUTime() syscall
+    else if (args->number == SYS_GETPROCINFO)
     {
         args->arg1 = readtime();
     }
 
-    if (args->number == SYS_GETPID)
+    // GetPID() syscall
+    else if (args->number == SYS_GETPID)
     {
         args->arg1 = getpid();
     }
